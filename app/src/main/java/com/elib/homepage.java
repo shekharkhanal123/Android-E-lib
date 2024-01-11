672bd2eaf41a39ce78f2bdcafa9d8f9808298c05
shekharkhanal123 <shekharkhanal123@gmail.com>
2024-01-11 15:18:31 +0545

    updated

--- app/src/main/java/com/elib/homepage.java
+++ app/src/main/java/com/elib/homepage.java
@@ -2,13 +2,40 @@ package com.elib;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class homepage extends AppCompatActivity {
+
+    Button dip,man,sci;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        dip = findViewById(R.id.dipbtn);
+        man = findViewById(R.id.manbtn);
+        sci = findViewById(R.id.scibtn);
+
+
+        //        Navigation
+        Intent navdip = new Intent(homepage.this, diploma.class);
+        Intent navsci = new Intent(homepage.this, management.class);
+        Intent navman = new Intent(homepage.this, science.class);
+
+        dip.setOnClickListener(v -> {
+            startActivity(navdip);
+        });
+
+        sci.setOnClickListener(v -> {
+            startActivity(navsci);
+        });
+
+        man.setOnClickListener(v -> {
+            startActivity(navman);
+        });
+
     }
 }
